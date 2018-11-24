fa91b7a8624eb24de55207062e559b3170c57412
Thomas Ridings <thomas.a.ridings@gmail.com>
2018-11-24 16:06:34 +0000

    adding URL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='jupyterhub-ec2spawner',
     version='0.1',
     description='Simple EC2 spawner for JupyterHub',
-    url='',
+    url='https://github.com/Ridingst/jupyterhub-ec2spawner',
     author='Thomas Ridings',
     author_email='mail@thomasridings.com',
     license='3 Clause BSD',
