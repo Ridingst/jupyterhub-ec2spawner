2df4779d890eb379b37e12664522ac59e9937261
Thomas Ridings <thomas.a.ridings@gmail.com>
2018-11-25 23:21:15 +0000

    adding bootstrap to build'

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup
 
 setup(
     name='jupyterhub-ec2spawner',
-    version='0.8',
+    version='0.10',
     description='Simple EC2 spawner for JupyterHub',
     url='https://github.com/Ridingst/jupyterhub-ec2spawner',
     author='Thomas Ridings',
     author_email='mail@thomasridings.com',
     license='3 Clause BSD',
     packages=['EC2Spawner'],
-    scripts=['./EC2Spawner/bootstrap.sh'],
     install_requires=[
       'setuptools',
       'jupyterhub',
