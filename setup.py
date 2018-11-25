459ae3ef2fda48fe1c8cca330d8231cb1beb888e
Thomas Ridings <thomas.a.ridings@gmail.com>
2018-11-25 22:24:47 +0000

    fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='jupyterhub-ec2spawner',
-    version='0.6',
+    version='0.7',
     description='Simple EC2 spawner for JupyterHub',
     url='https://github.com/Ridingst/jupyterhub-ec2spawner',
     author='Thomas Ridings',
